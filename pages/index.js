b012085487274e6291a169a931ae09f011c95a07
newton099 <87276295+newton099@users.noreply.github.com>
2023-02-07 13:57:34 -0600

    commit

--- pages/index.js
+++ pages/index.js
@@ -33,7 +33,7 @@ export default function Home() {
               <li>
                 <BsFillMoonStarsFill className=" cursor-pointer text-2xl"/>
               </li>
-                  <li>  <a className= "bg-gradient-to-r from-cyan-500 text- to-teal-500 text-white px-4 py-2 border-none rounded-md ml-8"
+                  <li>  <a className= "bg-gradient-to-r from-amber-500 text- to-yellow-500 text-white px-4 py-2 border-none rounded-md ml-8"
                     href="#"> Resume </a>
                   </li>
             </ul>
@@ -41,7 +41,7 @@ export default function Home() {
           </nav>
 
           <div className="text-center p-10 py-10">
-              <h2 className="text-5xl py-2 text-teal-600 font-medium dark:text-teal-400 md:text-6xl">Eder M.
+              <h2 className="text-5xl font-burtons py-2 text-orange-400 font-medium dark:text-orange-400 md:text-6xl">Eder M.
               </h2>
               <h3 className="text-2xl py-2 dark:text-white md:text-3xl">
               Web3 Developer.
@@ -58,7 +58,7 @@ export default function Home() {
             <AiFillGithub/>
           </div>
 
-          <div className="mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 relative overflow-hidden mt-20 md:h-96 md:w-96">
+          <div className="mx-auto bg-gradient-to-b from-yellow-500 rounded-full w-80 h-80 relative overflow-hidden mt-20 md:h-96 md:w-96">
           <Image src={yo} layout="fill" objectFit="cover" />
           </div>
         </section>

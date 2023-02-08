f775cb85abf80cd6e0e341f9763b59f04706e725
newton099 <87276295+newton099@users.noreply.github.com>
2023-02-08 17:12:24 -0600

    commit

--- pages/index.js
+++ pages/index.js
@@ -29,20 +29,23 @@ export default function Home() {
           
             
             <nav className="py-10 mb-12 flex justify-between">
-              <h1 className=" tex-xl font-burtons ">EderCrypto</h1>
+              <h1 className=" tex-xl font-burton py-3 font-sans ">EderCrypto.eth</h1>
               <ul className=" flex items-center">
+                <li><a>Projects</a></li>
+                <li><a>Abaout</a></li>
                 <li>
                   <BsFillMoonStarsFill onClick={() => setDarkMode(!darkMode)} className=" cursor-pointer text-2xl"/>
                 </li>
-                    <li>  <a className= "bg-gradient-to-r from-amber-500 text- to-yellow-500 text-white px-4 py-2 border-none rounded-md ml-8"
+                <li>  <a className= "bg-gradient-to-r from-amber-500 text- to-yellow-500 text-white px-4 py-2 border-none rounded-md ml-8"
                       href="#"> Resume </a>
-                    </li>
+                </li>
+
               </ul>
 
             </nav>
 
             <div className="text-center p-10 py-10">
-                <h2 className="text-5xl font-burtons py-2 text-orange-400 font-medium dark:text-orange-400 md:text-6xl">Eder M.
+                <h2 className="text-5xl font-akira py-2 text-orange-400 font-medium dark:text-orange-400 md:text-6xl">Eder M.
                 </h2>
                 <h3 className="text-2xl py-2 dark:text-white md:text-3xl">
                 Web3 Developer.
